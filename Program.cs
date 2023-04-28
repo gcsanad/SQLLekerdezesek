d13c60c09188b25ce9a419bcc3211f83e6541d8a
gcsanad <71721197+gcsanad@users.noreply.github.com>
2023-04-28 12:59:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using MySql.Data.MySqlClient;
+
+string kapcsolatLeiro = "datasource=127.0.0.1;port=3306;database=hardver;username=root;password=;charset=utf8;";
+MySqlConnection SQLKapcsolat = new MySqlConnection(kapcsolatLeiro);
+
+try
+{
+    SQLKapcsolat.Open();
+}
+catch (MySqlException hiba)
+{
+    Console.WriteLine(hiba.Message);
+    Environment.Exit(1);
+}
+
+
+
+string SQLLekerdezesKategoriakraRendezetten = "SELECT DISTINCT * FROM `termékek` WHERE Ár > (SELECT Ár FROM termékek WHERE Ár = (SELECT MAX(Ár) FROM termékek WHERE Kategória LIKE 'winchester%' AND Gyártó LIKE 'seagate%')) AND Kategória LIKE 'winchester%' ORDER BY termékek.Ár ASC;";
+
+MySqlCommand SQLparancs = new MySqlCommand(SQLLekerdezesKategoriakraRendezetten, SQLKapcsolat);
+MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+
+
+while (eredmenyOlvaso.Read())
+{
+    string szoveg = "";
+    szoveg += eredmenyOlvaso.GetString("Cikkszám") + " | ";
+    szoveg += eredmenyOlvaso.GetString("Kategória") + " | ";
+    szoveg += eredmenyOlvaso.GetString("Gyártó") + " | ";
+    szoveg += eredmenyOlvaso.GetString("Név") + " | ";
+    szoveg += eredmenyOlvaso.GetString("Ár") + " | ";
+    szoveg += eredmenyOlvaso.GetString("Garidő") + " | ";
+    szoveg += eredmenyOlvaso.GetString("Készlet") + " | ";
+    szoveg += eredmenyOlvaso.GetString("Súly");
+    
+    Console.WriteLine(szoveg);
+}
+eredmenyOlvaso.Close();
+SQLKapcsolat.Close();
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * 1.FELADAT
+ * 
+string SQLLekerdezesKategoriakraRendezetten = "SELECT DISTINCT Gyártó FROM termékek GROUP BY Gyártó HAVING COUNT(Kategória = 'monitor') >= 40;";
+
+MySqlCommand SQLparancs = new MySqlCommand(SQLLekerdezesKategoriakraRendezetten, SQLKapcsolat);
+MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+
+
+while (eredmenyOlvaso.Read())
+{
+    Console.WriteLine(eredmenyOlvaso.GetString("Gyártó"));
+}
+eredmenyOlvaso.Close();
+SQLKapcsolat.Close();
+*/
+
+
+
+
+
+
+
+
+
